5282b2f6c29a5b7b3a87435f25d0b0bce77b87c1
kevinarce2 <kevin.arce@opolarity.com>
2026-01-21 12:08:56 -0500

    delet-shipping-method

---
+++ supabase/functions/delete-shipping-method/index.ts
@@ -0,0 +1,79 @@
+import "https://deno.land/x/xhr@0.1.0/mod.ts";
+import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+
+const corsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+};
+
+serve(async (req) => {
+  // Handle CORS preflight requests
+  if (req.method === 'OPTIONS') {
+    return new Response(null, { headers: corsHeaders });
+  }
+
+  try {
+    const { methodID } = await req.json();
+
+    if (!methodID) {
+      return new Response(
+        JSON.stringify({ error: 'Method ID is required' }),
+        {
+          headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+          status: 400
+        }
+      );
+    }
+
+    console.log('Deleting method:', methodID);
+
+    const supabaseUrl = Deno.env.get('SUPABASE_URL')!;
+    const supabaseKey = Deno.env.get('SUPABASE_SERVICE_ROLE_KEY')!;
+    const supabase = createClient(supabaseUrl, supabaseKey);
+
+    // First, delete all product_categories records for this category
+    const { error: deleteshippincost } = await supabase
+      .from('shipping_costs')
+      .delete()
+      .eq('shipping_method_id', methodID);
+
+    if (deleteshippincost) {
+      console.error('Error deleting product_categories:', deleteshippincost);
+      throw deleteshippincost;
+    }
+
+    console.log('Deleted shipping_cost records');
+
+    // Then delete the category itself
+    const { error: deleteMethodShipping } = await supabase
+      .from('shipping_methods')
+      .delete()
+      .eq('id', methodID);
+
+    if (deleteMethodShipping) {
+      console.error('Error deleting category:', deleteMethodShipping);
+      throw deleteMethodShipping;
+    }
+
+    console.log('Method Shipping deleted successfully');
+
+    return new Response(
+      JSON.stringify({ success: true }),
+      {
+        headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+        status: 200
+      }
+    );
+
+  } catch (error) {
+    console.error('Error in delete-method-shipping function:', error);
+    return new Response(
+      JSON.stringify({ error: error.message }),
+      {
+        headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+        status: 500
+      }
+    );
+  }
+});
