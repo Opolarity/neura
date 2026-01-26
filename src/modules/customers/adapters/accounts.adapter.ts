df7828b319b6519aafebf8052abf31fb951b1c1b
kevinarce2 <kevin.arce@opolarity.com>
2026-01-26 17:59:54 -0500

    customers list

---
+++ src/modules/customers/adapters/accounts.adapter.ts
@@ -0,0 +1,30 @@
+import { PaginationState } from "@/shared/components/pagination/Pagination";
+import { Account, AccountsApiResponse } from '../types/accounts.types';
+
+export const accountsAdapter = (response: AccountsApiResponse) => {
+    const formattedAccounts: Account[] = response.accountsdata.data.map((item): Account => {
+        const nameParts = [
+            item.name,
+            item.middle_name,
+            item.last_name,
+            item.last_name2,
+        ].filter(Boolean);
+
+        return {
+            id: item.id,
+            fullName: nameParts.join(' '),
+            documentNumber: item.document_number,
+            typeName: item.types_name,
+            show: item.show,
+            documentTypeId: item.document_type_id,
+        };
+    });
+
+    const pagination: PaginationState = {
+        p_page: response.accountsdata.page.page,
+        p_size: response.accountsdata.page.size,
+        total: response.accountsdata.page.total,
+    };
+
+    return { data: formattedAccounts, pagination };
+};
--- src/modules/customers/index.ts
+++ src/modules/customers/index.ts
@@ -1,5 +1,5 @@
 export { customersRoutes } from './routes.tsx';
 export { default as Customers } from './pages/Customers';
-export { default as ClientsList } from './pages/ClientsList';
+export { default as AccountsList } from './pages/AccountsList';
 export { default as CreateClient } from './pages/CreateClient';
 export { default as EditClient } from './pages/EditClient';
