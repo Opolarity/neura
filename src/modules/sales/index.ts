5a595ff7a38141aef25b54978fd579358cd88d91
Diego Osorio <dosoriohorna@gmail.com>
2026-01-27 10:37:28 -0500

    delete viewsales.tsx

--- src/modules/sales/index.ts
+++ src/modules/sales/index.ts
@@ -2,5 +2,5 @@ export { salesRoutes } from './routes.tsx';
 export { default as Sales } from './pages/Sales';
 export { default as SalesList } from './pages/SalesList';
 export { default as CreateSale } from './pages/CreateSale';
-export { default as ViewSale } from './pages/ViewSale';
+
 export { SaleSidebar } from './components/SaleSidebar';
