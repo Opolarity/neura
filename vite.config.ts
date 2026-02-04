7e0b0e23b7af3a6bd05b68f56a18770993f6ec39
waldo-liberato-jara <waldo.liberato@opolarity.com>
2026-02-04 17:20:19 -0500

    vite.config.ts lovable

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-
-export default defineConfig(() => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@app": path.resolve(__dirname, "./src/app"),
-      "@modules": path.resolve(__dirname, "./src/modules"),
-      "@shared": path.resolve(__dirname, "./src/shared"),
-    },
-  },
-}));
-
-
-/*
-
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
@@ -42,5 +19,24 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
+/*
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
+export default defineConfig(() => ({
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@app": path.resolve(__dirname, "./src/app"),
+      "@modules": path.resolve(__dirname, "./src/modules"),
+      "@shared": path.resolve(__dirname, "./src/shared"),
+    },
+  },
+}));
 */
