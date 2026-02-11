970bb285ebb0148de84277cfd13823945b62e6d7
waldo-liberato-jara <waldo.liberato@opolarity.com>
2026-02-11 15:41:53 -0500

    front:price-list table

--- src/modules/price-list/index.ts
+++
@@ -1,1 +0,0 @@
-export { priceListRoutes } from "./routes";
--- src/modules/settings/routes.tsx
+++ src/modules/settings/routes.tsx
@@ -1,32 +1,34 @@
-import { RouteObject, Outlet } from 'react-router-dom';
-import Settings from './pages/Settings';
-import UsersList from './pages/UsersList';
-import CreateUser from './pages/CreateUser';
-import UserFunctions from './pages/UserFunctions';
-import RolesList from './pages/RolesList';
-import CreateRole from './pages/CreateRole';
-import WarehousesList from './pages/warehouses';
-import CreateWarehouses from './pages/CreateWarehouses';
-import BranchesList from './pages/branches';
-import CreateBranch from './pages/CreateBranch';
+import { RouteObject, Outlet } from "react-router-dom";
+import Settings from "./pages/Settings";
+import UsersList from "./pages/UsersList";
+import CreateUser from "./pages/CreateUser";
+import UserFunctions from "./pages/UserFunctions";
+import RolesList from "./pages/RolesList";
+import CreateRole from "./pages/CreateRole";
+import WarehousesList from "./pages/warehouses";
+import CreateWarehouses from "./pages/CreateWarehouses";
+import BranchesList from "./pages/branches";
+import CreateBranch from "./pages/CreateBranch";
+import PriceListPage from "./pages/PriceListPage";
 
 export const settingsRoutes: RouteObject[] = [
   {
-    path: 'settings',
+    path: "settings",
     element: <Settings />,
     children: [
-      { path: 'users', element: <UsersList /> },
-      { path: 'users/create', element: <CreateUser /> },
-      { path: 'users/functions', element: <UserFunctions /> },
-      { path: 'roles', element: <RolesList /> },
-      { path: 'roles/create', element: <CreateRole /> },
-      { path: 'roles/edit/:id', element: <CreateRole /> },
-      { path: 'warehouses', element: <WarehousesList /> },
-      { path: 'warehouses/create', element: <CreateWarehouses /> },
-      { path: 'warehouses/edit/:id', element: <CreateWarehouses /> },
-      { path: 'branches', element: <BranchesList /> },
-      { path: 'branches/create', element: <CreateBranch /> },
-      { path: 'branches/edit/:id', element: <CreateBranch /> },
-    ]
+      { path: "price-list", element: <PriceListPage /> },
+      { path: "users", element: <UsersList /> },
+      { path: "users/create", element: <CreateUser /> },
+      { path: "users/functions", element: <UserFunctions /> },
+      { path: "roles", element: <RolesList /> },
+      { path: "roles/create", element: <CreateRole /> },
+      { path: "roles/edit/:id", element: <CreateRole /> },
+      { path: "warehouses", element: <WarehousesList /> },
+      { path: "warehouses/create", element: <CreateWarehouses /> },
+      { path: "warehouses/edit/:id", element: <CreateWarehouses /> },
+      { path: "branches", element: <BranchesList /> },
+      { path: "branches/create", element: <CreateBranch /> },
+      { path: "branches/edit/:id", element: <CreateBranch /> },
+    ],
   },
 ];
