ded08ce456c798c544b204bcf08297294a0ecd79
waldo-liberato-jara <waldo.liberato@opolarity.com>
2026-03-09 14:06:14 -0500

    no importa

--- src/modules/ecommerce/hooks/useEcommerceSso.ts
+++ src/modules/ecommerce/hooks/useEcommerceSso.ts
@@ -2,7 +2,8 @@ import { useState } from "react";
 import { toast } from "sonner";
 import { generateSSOToken } from "../services/sso.service";
 
-const ECOMMERCE_SSO_URL = "https://ecommerce.neura.pe/editor";
+//const ECOMMERCE_SSO_URL = "https://ecommerce.neura.pe/editor";
+const ECOMMERCE_SSO_URL = "http://localhost:3000/editor";
 
 export const useEcommerceSso = () => {
   const [loading, setLoading] = useState(false);
@@ -11,10 +12,12 @@ export const useEcommerceSso = () => {
     setLoading(true);
     try {
       const { token } = await generateSSOToken();
-      window.open(`${ECOMMERCE_SSO_URL}?token=${token}`, '_blank');
+      window.open(`${ECOMMERCE_SSO_URL}?token=${token}`, "_blank");
     } catch (error) {
       const message =
-        error instanceof Error ? error.message : "Error al redirigir al ecommerce";
+        error instanceof Error
+          ? error.message
+          : "Error al redirigir al ecommerce";
       toast.error(message);
     } finally {
       setLoading(false);
